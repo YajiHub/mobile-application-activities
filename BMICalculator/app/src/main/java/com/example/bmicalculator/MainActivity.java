b5a746d2494624eb583e8cf5b29c4e1bc57b2ed0
YajiHub <jopurjay23@gmail.com>
2025-09-12 06:15:34 +0800

    Update with the challenge version and feature

--- BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.bmicalculator;
 
+import java.util.HashMap;
+import java.util.Map;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,6 +14,9 @@ import android.widget.TextView;
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+    double nextCat = 0;
+    double prevCat = 0;
+    Double[] bmi_categories = {15.0, 16.0, 18.5, 25.0, 30.0, 35.0, 40.0};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
+        TextView prev = findViewById(R.id.prevCat);
+        TextView next = findViewById(R.id.nextCat);
+
+        prev.setVisibility(View.INVISIBLE);
+        next.setVisibility(View.INVISIBLE);
+
     }
 
     public void calculateBMI(){
@@ -43,22 +54,138 @@ public class MainActivity extends AppCompatActivity {
         String bmi_string;
         String BMI_Cat;
 
-        if (BMI < 15)
+        if (BMI < 15) {
+            nextCat = bmi_categories[1] - 15;
+            double next_trim = Double.parseDouble(df.format(nextCat));
             BMI_Cat = "Very severely underweight";
-        else if (BMI < 16)
+
+            TextView next = findViewById(R.id.nextCat);
+            String message = "Gain " +  next_trim + " to be severely underweight.";
+            next.setText(message);
+            next.setVisibility(View.VISIBLE);
+        }
+        else if (BMI < 16) {
+            nextCat = bmi_categories[2] - BMI;
+            prevCat = BMI - bmi_categories[0];
+            double next_trim = Double.parseDouble(df.format(nextCat));
+            double prev_trim = Double.parseDouble(df.format(prevCat));
+
+
             BMI_Cat = "Severely underweight";
-        else if (BMI < 18.5)
+
+            TextView next = findViewById(R.id.nextCat);
+            String message = "Gain " +  next_trim + " to be underweight.";
+            next.setText(message);
+            next.setVisibility(View.VISIBLE);
+
+            TextView prev = findViewById(R.id.prevCat);
+            String message_prev = "Lose " +  prev_trim + " to be very severely underweight.";
+            prev.setText(message_prev);
+            prev.setVisibility(View.VISIBLE);
+
+        }
+        else if (BMI < 18.5) {
+            nextCat = bmi_categories[3] - BMI;
+            prevCat = BMI - bmi_categories[1];
+            double next_trim = Double.parseDouble(df.format(nextCat));
+            double prev_trim = Double.parseDouble(df.format(prevCat));
+
             BMI_Cat = "Underweight";
-        else if (BMI < 25)
+
+            TextView next = findViewById(R.id.nextCat);
+            String message = "Gain " +  next_trim + " to be Normal.";
+            next.setText(message);
+            next.setVisibility(View.VISIBLE);
+
+            TextView prev = findViewById(R.id.prevCat);
+            String message_prev = "Lose " +  prev_trim + " to be severely underweight.";
+            prev.setText(message_prev);
+            prev.setVisibility(View.VISIBLE);
+        }
+        else if (BMI < 25) {
+            nextCat = bmi_categories[4] - BMI;
+            prevCat = BMI - bmi_categories[2];
+            double next_trim = Double.parseDouble(df.format(nextCat));
+            double prev_trim = Double.parseDouble(df.format(prevCat));
+
             BMI_Cat = "Normal";
-        else if (BMI < 30)
+
+            TextView next = findViewById(R.id.nextCat);
+            String message = "Gain " +  next_trim + " to be Overweight.";
+            next.setText(message);
+            next.setVisibility(View.VISIBLE);
+
+            TextView prev = findViewById(R.id.prevCat);
+            String message_prev = "Lose " +  prev_trim + " to be underweight.";
+            prev.setText(message_prev);
+            prev.setVisibility(View.VISIBLE);
+
+        }
+        else if (BMI < 30) {
+
+            nextCat = bmi_categories[5] - BMI;
+            prevCat = BMI - bmi_categories[3];
+            double next_trim = Double.parseDouble(df.format(nextCat));
+            double prev_trim = Double.parseDouble(df.format(prevCat));
+
             BMI_Cat = "Overweight";
-        else if (BMI < 35)
+
+            TextView next = findViewById(R.id.nextCat);
+            String message = "Gain " +  next_trim + " to be Obese Class 1 - Moderately Obese.";
+            next.setText(message);
+            next.setVisibility(View.VISIBLE);
+
+            TextView prev = findViewById(R.id.prevCat);
+            String message_prev = "Lose " +  prev_trim + " to be Normal.";
+            prev.setText(message_prev);
+            prev.setVisibility(View.VISIBLE);
+        }
+        else if (BMI < 35) {
+            nextCat = bmi_categories[6] - BMI;
+            prevCat = BMI - bmi_categories[4];
+            double next_trim = Double.parseDouble(df.format(nextCat));
+            double prev_trim = Double.parseDouble(df.format(prevCat));
+
             BMI_Cat = "Obese Class 1 - Moderately Obese";
-        else if (BMI < 40)
+
+            TextView next = findViewById(R.id.nextCat);
+            String message = "Gain " +  next_trim + " to be Obese Class 2 - Severely Obese.";
+            next.setText(message);
+            next.setVisibility(View.VISIBLE);
+
+            TextView prev = findViewById(R.id.prevCat);
+            String message_prev = "Lose " +  prev_trim + "  to be Overweight.";
+            prev.setText(message_prev);
+            prev.setVisibility(View.VISIBLE);
+        }
+        else if (BMI < 40) {
+            nextCat = bmi_categories[7] - BMI;
+            prevCat = BMI - bmi_categories[5];
+            double next_trim = Double.parseDouble(df.format(nextCat));
+            double prev_trim = Double.parseDouble(df.format(prevCat));
+
             BMI_Cat = "Obese Class 2 - Severely Obese";
+
+            TextView next = findViewById(R.id.nextCat);
+            String message = "Gain " +  next_trim + " to be Obese Class 3 - Very Severely Obese.";
+            next.setText(message);
+            next.setVisibility(View.VISIBLE);
+
+            TextView prev = findViewById(R.id.prevCat);
+            String message_prev = "Lose " +  prev_trim + "  Obese Class 1 - Moderately Obese.";
+            prev.setText(message_prev);
+            prev.setVisibility(View.VISIBLE);
+        }
         else{
+            prevCat = BMI - bmi_categories[6];
+            double prev_trim = Double.parseDouble(df.format(prevCat));
+
             BMI_Cat = "Obese Class 3 - Very Severely Obese";
+
+            TextView prev = findViewById(R.id.prevCat);
+            String message_prev = "Lose " +  prev_trim + "  Obese Class 2 - Severely Obese.";
+            prev.setText(message_prev);
+            prev.setVisibility(View.VISIBLE);
         }
 
         bmi_category.setText(BMI_Cat);
