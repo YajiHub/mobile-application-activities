f0ec78a855bf0c20045f9017aecb9127d2068dd3
YajiHub <jopurjay23@gmail.com>
2025-09-12 06:43:43 +0800

    Update initial not complete-yet version with addition of challenge

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.diceroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -10,14 +11,28 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView dice1;
+    ImageView dice2;
+    TextView text1;
+    TextView text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dice1 = (ImageView) findViewById(R.id.diceImage);
+        dice2 = (ImageView) findViewById(R.id.diceImage1);
+        text1 = (TextView) findViewById(R.id.text1);
+        text2 = (TextView) findViewById(R.id.text2);
+        text1.setVisibility(View.INVISIBLE);
+        text2.setVisibility(View.INVISIBLE);
+
 
         roll_dice();
     }
@@ -27,13 +42,24 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Random random = new Random();
-                int num = random.nextInt(6) + 1;
-                ImageView dice_image = (ImageView) findViewById(R.id.diceImage);
-                TextView diceResult = (TextView) findViewById(R.id.text);
-                diceResult.setText(Integer.toString(num));
+                int dice1_num;
+                int dice2_num;
 
-                switch (num) {
+                Random random = new Random();
+                dice1_num = random.nextInt(6) + 1;
+                dice2_num = random.nextInt(6) + 1;
+
+                ImageView dice_image = dice1;
+
+                text1.setText(Integer.toString(dice1_num));
+                text2.setText(Integer.toString(dice2_num));
+                text1.setVisibility(View.VISIBLE);
+                text2.setVisibility(View.VISIBLE);
+
+                TextView tap_text = findViewById(R.id.text);
+                tap_text.setVisibility(View.INVISIBLE);
+
+                switch (dice1_num) {
                     case 1:
                         dice_image.setImageResource(R.drawable.dice1);
                         break;
@@ -51,6 +77,28 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case 6:
                         dice_image.setImageResource(R.drawable.dice6);
+                        break;
+
+                }
+
+                switch (dice2_num) {
+                    case 1:
+                        dice2.setImageResource(R.drawable.dice1);
+                        break;
+                    case 2:
+                        dice2.setImageResource(R.drawable.dice2);
+                        break;
+                    case 3:
+                        dice2.setImageResource(R.drawable.dice3);
+                        break;
+                    case 4:
+                        dice2.setImageResource(R.drawable.dice4);
+                        break;
+                    case 5:
+                        dice2.setImageResource(R.drawable.dice5);
+                        break;
+                    case 6:
+                        dice2.setImageResource(R.drawable.dice6);
                         break;
 
                 }
