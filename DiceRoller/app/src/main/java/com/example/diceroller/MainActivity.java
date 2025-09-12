a39809f9e6bc757ee29873a5296a6cc821244c46
YajiHub <jopurjay23@gmail.com>
2025-09-12 08:35:44 +0800

    update to version-3 of the dice roller with the challenge animations and color edges

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
@@ -2,24 +2,33 @@ package com.example.diceroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-import android.graphics.Color;
-import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
     ImageView dice1;
     ImageView dice2;
     TextView text1;
     TextView text2;
+    private int[] frames;
+    private int[] colors;
+    private Handler animation_handler;
+    int current_frame = 0;
+    Runnable runnable;
+
+    int dice1_num;
+    int dice2_num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,75 +42,109 @@ public class MainActivity extends AppCompatActivity {
         text1.setVisibility(View.INVISIBLE);
         text2.setVisibility(View.INVISIBLE);
 
+        frames = new int[] {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+
+        // Define some fun colors for animation
+        colors = new int[] {
+                Color.BLACK,
+                Color.RED,
+                Color.BLUE,
+                Color.GRAY,
+                Color.CYAN,
+                Color.WHITE
+        };
 
         roll_dice();
     }
 
-    public void roll_dice(){
+    public void dice_animation() {
+        dice1.setImageResource(frames[current_frame]);
+        dice2.setImageResource(frames[current_frame]);
+
+        int color_index = current_frame % colors.length;
+        dice1.setBackgroundColor(colors[color_index]);
+        dice2.setBackgroundColor(colors[(color_index + 1) % colors.length]);
+
+        current_frame = (current_frame + 1) % frames.length;
+        animation_handler.postDelayed(runnable, 100);
+    }
+
+    public void destroy_animation() {
+        animation_handler.removeCallbacks(runnable);
+    }
+
+    public void show_final_result() {
+        text1.setText(Integer.toString(dice1_num));
+        text2.setText(Integer.toString(dice2_num));
+        text1.setVisibility(View.VISIBLE);
+        text2.setVisibility(View.VISIBLE);
+
+        dice1.setColorFilter(null);
+        dice2.setColorFilter(null);
+
+        switch (dice1_num) {
+            case 1: dice1.setImageResource(R.drawable.dice1); break;
+            case 2: dice1.setImageResource(R.drawable.dice2); break;
+            case 3: dice1.setImageResource(R.drawable.dice3); break;
+            case 4: dice1.setImageResource(R.drawable.dice4); break;
+            case 5: dice1.setImageResource(R.drawable.dice5); break;
+            case 6: dice1.setImageResource(R.drawable.dice6); break;
+        }
+        switch (dice2_num) {
+            case 1: dice2.setImageResource(R.drawable.dice1); break;
+            case 2: dice2.setImageResource(R.drawable.dice2); break;
+            case 3: dice2.setImageResource(R.drawable.dice3); break;
+            case 4: dice2.setImageResource(R.drawable.dice4); break;
+            case 5: dice2.setImageResource(R.drawable.dice5); break;
+            case 6: dice2.setImageResource(R.drawable.dice6); break;
+        }
+    }
+
+    public void roll_dice() {
         Button button = findViewById(R.id.rollButton);
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int dice1_num;
-                int dice2_num;
+                animation_handler = new Handler();
+                current_frame = 0;
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        dice_animation();
+                    }
+                };
+                animation_handler.post(runnable);
 
                 Random random = new Random();
                 dice1_num = random.nextInt(6) + 1;
                 dice2_num = random.nextInt(6) + 1;
 
-                ImageView dice_image = dice1;
-
-                text1.setText(Integer.toString(dice1_num));
-                text2.setText(Integer.toString(dice2_num));
-                text1.setVisibility(View.VISIBLE);
-                text2.setVisibility(View.VISIBLE);
-
+                text1.setVisibility(View.INVISIBLE);
+                text2.setVisibility(View.INVISIBLE);
                 TextView tap_text = findViewById(R.id.text);
                 tap_text.setVisibility(View.INVISIBLE);
 
-                switch (dice1_num) {
-                    case 1:
-                        dice_image.setImageResource(R.drawable.dice1);
-                        break;
-                    case 2:
-                        dice_image.setImageResource(R.drawable.dice2);
-                        break;
-                    case 3:
-                        dice_image.setImageResource(R.drawable.dice3);
-                        break;
-                    case 4:
-                        dice_image.setImageResource(R.drawable.dice4);
-                        break;
-                    case 5:
-                        dice_image.setImageResource(R.drawable.dice5);
-                        break;
-                    case 6:
-                        dice_image.setImageResource(R.drawable.dice6);
-                        break;
-
-                }
-
-                switch (dice2_num) {
-                    case 1:
-                        dice2.setImageResource(R.drawable.dice1);
-                        break;
-                    case 2:
-                        dice2.setImageResource(R.drawable.dice2);
-                        break;
-                    case 3:
-                        dice2.setImageResource(R.drawable.dice3);
-                        break;
-                    case 4:
-                        dice2.setImageResource(R.drawable.dice4);
-                        break;
-                    case 5:
-                        dice2.setImageResource(R.drawable.dice5);
-                        break;
-                    case 6:
-                        dice2.setImageResource(R.drawable.dice6);
-                        break;
-
-                }
+                Timer timer1 = new Timer();
+                TimerTask task1 = new TimerTask() {
+                    @Override
+                    public void run() {
+                        destroy_animation();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                show_final_result();
+                            }
+                        });
+                    }
+                };
+                timer1.schedule(task1, 2000);
             }
         });
     }
